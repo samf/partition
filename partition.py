11b1c61df2123fa3fc8ac664ce4c5a037a243d0c
Sam Falkner <sam.falkner@gmail.com>
2022-10-19 11:50:48 -0600

    option to rename the directories

--- partition.py
+++ partition.py
@@ -5,27 +5,57 @@ import os.path
 import shutil
 
 
-def partition(max_size, source_dir, dest_dir):
-    dcount = 0
-    current_size = 0
-    max_blocks = max_size << 11  # 1 for 512 to 1K, 10 more for K to M
+class DestDir:
+    dir_number = 0
 
-    final_dir = dname(dest_dir, dcount)
-    for fent in sorted(os.scandir(source_dir), key=lambda e: e.name):
-        if current_size + fent.stat().st_blocks > max_blocks:
-            current_size = 0
-            dcount += 1
-            final_dir = dname(dest_dir, dcount)
+    def __init__(self, parent_dir):
+        DestDir.dir_number += 1
+        self.name = f"{DestDir.dir_number:03}"
+        self.parent_dir = parent_dir
+        self.path = os.path.join(parent_dir, self.name)
+
+        self.blocks = 0
+        self.first = None
+        self.last = None
+
+        self.makedirs()
+
+    def copy2(self, source_path, blocks):
+        "copy source_path into this DestDir"
+        name = os.path.basename(source_path)
+        shutil.copy2(source_path, self.path)
+        self.blocks += blocks
+        if not self.first:
+            self.first = name
+        self.last = name
+
+    def makedirs(self):
+        os.makedirs(self.path)
+
+    def rename(self):
+        first = os.path.splitext(self.first)[0].replace("-", "_")
+        last = os.path.splitext(self.last)[0].replace("-", "_")
+        new_name = f"{first}-{last}"
+        new_path = os.path.join(self.parent_dir, new_name)
+        os.rename(self.path, new_path)
+
+
+def partition(max_size, source_dir, dest_parent, rename):
+    max_blocks = max_size << 11  # 1 for 512 to 1K, 10 more for K to M
+    dest = DestDir(dest_parent)
+
+    for fent in sorted(os.scandir(source_dir), key=lambda e: str.lower(e.name)):
+        blocks = fent.stat().st_blocks
+        if dest.blocks + blocks > max_blocks:
+            if rename:
+                dest.rename()
+            dest = DestDir(dest_parent)
 
         src_file = os.path.join(source_dir, fent.name)
-        shutil.copy2(src_file, final_dir)
-        current_size += fent.stat().st_blocks
+        dest.copy2(src_file, blocks)
 
-
-def dname(dest_dir, counter):
-    ddir = os.path.join(dest_dir, f"{counter:03}")
-    os.makedirs(ddir)
-    return ddir
+    if rename:
+        dest.rename()
 
 
 def dir_path(dname):
@@ -50,11 +80,18 @@ cli.add_argument(
     default="23",
     help="maximum size of each subdir in megabytes",
 )
+cli.add_argument(
+    "--rename-dirs",
+    "-r",
+    type=bool,
+    action=argparse.BooleanOptionalAction,
+    help="rename created dirs based on file names within",
+)
 
 
 def main():
     args = cli.parse_args()
-    partition(args.max_size, args.source_dir, args.dest_dir)
+    partition(args.max_size, args.source_dir, args.dest_dir, args.rename_dirs)
 
 
 if __name__ == "__main__":
